751db8c39b221223b7e5321bf1a5161910e4364f
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-07-21 13:34:34 -0300

    Persistindo uma entidade

---
+++ src/main/java/br/com/msaorim/loja/testes/Teste.java
@@ -0,0 +1,24 @@
+package br.com.msaorim.loja.testes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.msaorim.loja.entities.Produto;
+import br.com.msaorim.loja.repositories.ProdutoRepository;
+
+@Configuration
+public class Teste implements CommandLineRunner {
+
+	@Autowired
+	private ProdutoRepository produtoRepository; 
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		var p1 = new Produto(null, "MacBook Pro-X", "Notebook da Apple", 10000.0);
+		produtoRepository.save(p1);
+		
+	}
+
+}
