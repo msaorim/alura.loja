4aed93bed3992e65d3cdd210292114fca74bb142
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-07-24 12:36:20 -0300

    Ajuste nas entradas. nextLine()

--- src/main/java/br/com/msaorim/loja/testes/Teste.java
+++ src/main/java/br/com/msaorim/loja/testes/Teste.java
@@ -60,6 +60,11 @@ public class Teste implements CommandLineRunner {
 				System.out.println("=================");
 				salvarProduto();
 				break;
+			case '3':
+				System.out.println("LISTA DOS PRODUTOS");
+				System.out.println("=================");
+				listarProduto();
+				break;
 			default: {
 				System.out.println("Opção inválida!!!");
 				break;
@@ -71,49 +76,71 @@ public class Teste implements CommandLineRunner {
 	}
 
 	public void salvarCategoria() {
+		sc.nextLine();
 		System.out.print("\nNome da Categoria: ");
-		String nome = sc.next();
+		String nome = sc.nextLine();
 		var cat1 = new Categoria(null, nome);
 		System.out.println("\nSalvando categoria: " + cat1.getNome() + "... salvo com sucesso!");
 		categoriaRepository.save(cat1);
 	}
-	
+
 	public void salvarProduto() {
-		System.out.print("\nNome do Produto: ");
-		String nome = sc.next();
-		
-		System.out.print("\nDescrição do Produto: ");
-		String descricao = sc.next();
-		
-		System.out.print("\nPreço do Produto: ");
-		Double preco = sc.nextDouble();
-		
-		System.out.println("\nCategoria do Produto: ");
-		List<Categoria> lista = new ArrayList<>();
-		lista = categoriaRepository.findAll();
-		for (Categoria c : lista) {
-			System.out.println(c.getId() + ": " + c.getNome());
+		if (categoriaRepository.findAll().isEmpty()) {
+			System.out.println("Nenhuma categoria cadastrada!!!");
+			System.out.println("Cadastrar categoria primeiro!!!");
+		} else {
+			sc.nextLine();
+			System.out.print("\nNome do Produto: ");
+			String nome = sc.nextLine();
+
+			System.out.print("\nDescrição do Produto: ");
+			String descricao = sc.nextLine();
+
+			System.out.print("\nPreço do Produto: ");
+			Double preco = sc.nextDouble();
+
+			System.out.println("\nCategoria do Produto: ");
+			List<Categoria> lista = new ArrayList<>();
+			lista = categoriaRepository.findAll();
+			for (Categoria c : lista) {
+				System.out.println(c.getId() + ": " + c.getNome());
+			}
+			boolean ficar = true;
+
+			while (ficar) {
+				System.out.print("\nEscolha uma Categoria: ");
+				long catEscolhida = sc.nextLong();
+				System.out.println("");
+				Optional<Categoria> obj = categoriaRepository.findById(catEscolhida);
+
+				if (obj.isPresent()) {
+					var p = new Produto(null, nome, descricao, preco, obj.get());
+					System.out.println(p.getId() + "\n" + p.getNome() + "\n" + p.getDescricao() + "\n" + p.getPreco()
+							+ "\n" + p.getCategoria().getNome());
+					produtoRepository.save(p);
+					ficar = false;
+				} else {
+					System.out.println("Categoria Inválida !!!!!");
+				}
+			}
 		}
-		boolean ficar = true;
-		
-		while (ficar) {
-			System.out.print("\nEscolha uma Categoria: ");
-			long catEscolhida = sc.nextLong();
-			System.out.println("");
-			Optional<Categoria> obj = categoriaRepository.findById(catEscolhida);
-			if(obj.isPresent()) {
-				var p = new Produto(null, nome, descricao, preco, obj.get());
-				System.out.println(p.getId() + "\n" +
-						p.getNome() + "\n" +
-						p.getDescricao() + "\n" +
-						p.getPreco() + "\n" +
-						p.getCategoria().getNome());
-				produtoRepository.save(p);
-				ficar = false;
-			}else {
-				System.out.println("Categoria Inválida !!!!!");
+
+	}
+
+	public void listarProduto() {
+		List<Produto> lista = new ArrayList<>();
+		lista = produtoRepository.findAll();
+		if (lista.isEmpty()) {
+			System.out.println("Nenhum produto para listar");
+		} else {
+			for (Produto p : lista) {
+				System.out.println("-------------------------------");
+				System.out.println("ID: " + p.getId());
+				System.out.println("Nome: " + p.getNome());
+				System.out.println("Descrição: " + p.getDescricao());
+				System.out.println("Preço: " + p.getPreco());
+				System.out.println("Categoria: " + p.getCategoria().getNome() + "\n");
 			}
 		}
 	}
-
 }
