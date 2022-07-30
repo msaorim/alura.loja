1446e98276bc17a812d6a3be86725248c56ec6db
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-07-29 23:08:23 -0300

    Add form produto

--- src/main/java/br/com/msaorim/loja/controllers/PedidoController.java
+++ src/main/java/br/com/msaorim/loja/controllers/PedidoController.java
@@ -4,23 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.com.msaorim.loja.entities.Pedido;
 import br.com.msaorim.loja.services.PedidoService;
 
-@RestController
-@RequestMapping(value = "/pedidos")
+@Controller
+@RequestMapping(value = "/pedido")
 public class PedidoController {
 
 	@Autowired
 	private PedidoService pedidoService;
 	
-	@GetMapping
+	@GetMapping(value = "/endpoint")
 	public ResponseEntity<List<Pedido>> findAll(){
 		List<Pedido> lista = pedidoService.findAll();
 		return ResponseEntity.ok().body(lista);
+	}
+	
+	@GetMapping(value = "/formulario")
+	public String formulario() {
+		return "pedido/formulario";
 	}
 }
